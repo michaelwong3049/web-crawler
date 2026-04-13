d6f77ab1e2b9d685fe0d69a0f96fa4a513fb3e42
michaelwong3049 <michaelwong3049@gmail.com>
2026-04-13 19:49:27 -0400

    integrate mongodb to save parsed urls

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+  "io"
+  "os"
 	"hash/fnv"
-	"io"
 	"net/http"
-	"strings"
 	"sync"
+  "bytes"
+  "strings"
+  "context"
+
 	"golang.org/x/net/html"
+  "github.com/joho/godotenv"
+  "go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type CrawledSet struct {
   Links map[uint64]struct{}
   mu sync.Mutex
+}
+
+type Website struct {
+  Url string
+  Title string
+}
+
+type DatabaseConnection struct {
+  url string
+  client *mongo.Client
+  collection *mongo.Collection
 }
 
 func NewCrawledSet() *CrawledSet {
@@ -22,17 +39,64 @@ func NewCrawledSet() *CrawledSet {
   }
 }
 
-const SITE_URL = "https://www.cc.gatech.edu/"
+func NewDatabaseConnection(connectionUrl string) *DatabaseConnection {
+  return &DatabaseConnection{
+    url: connectionUrl,
+    client: nil,
+    collection: nil,
+  }
+}
+
+func initializeDatabase(connectionUrl string) (db *DatabaseConnection, err error) {
+  db = NewDatabaseConnection(connectionUrl)
+
+  client, err := mongo.Connect(options.Client().ApplyURI(connectionUrl))
+
+  if err != nil {
+    return nil, err
+  }
+
+  db.client = client
+  db.collection = client.Database("web-crawler").Collection("websites")
+
+  return db, nil
+}
+
+const SITE_URL = "https://hunter.cuny.edu/"
 
 func main() {
   fmt.Println("--- runnning main ---")
+
+  err := godotenv.Load()
+  if err != nil {
+    panic(err)
+  }
+
+  MONGODB_URI := os.Getenv("MONGODB_URI")
+
+  database, err := initializeDatabase(MONGODB_URI)
+  if err != nil || database.client == nil {
+    fmt.Println("Error connecting to database")
+    return 
+  }
+
+  defer func() {
+    if err := database.client.Disconnect(context.TODO()); err != nil {
+      panic(err)
+    }
+  }()
+
+  fmt.Println(database.client)
+
   websites := make(chan string) 
   crawledSet := NewCrawledSet()
+  client := &http.Client{}
+
   var wg sync.WaitGroup
   for w := 0; w <= 4; w++ {
     wg.Add(1)
     go func() {
-      err := worker(w, websites, crawledSet, &wg)
+      err := worker(w, websites, crawledSet, database, client, &wg)
       if err != nil {
         fmt.Println("BIG ERROR FOUND: ", err)
         return
@@ -40,23 +104,26 @@ func main() {
     }()
   }
 
-  var url uint64 = hashUrl("https://www.cc.gatech.edu/")
+  var url uint64 = hashUrl(SITE_URL)
   crawledSet.mu.Lock() 
   crawledSet.Links[url] = struct{}{}
   crawledSet.mu.Unlock() 
-  websites <- "https://www.cc.gatech.edu/"
+  websites <- SITE_URL
 
   wg.Wait()
 }
 
-func worker(id int, websites chan string, cs *CrawledSet, wg *sync.WaitGroup) (err error) {
+func worker(id int, websites chan string, cs *CrawledSet, db *DatabaseConnection, client *http.Client, wg *sync.WaitGroup) (err error) {
   defer wg.Done()
 
   fmt.Println("worker: ", id)
 
   for site := range websites {
+    cs.mu.Lock()
+    fmt.Print("length: ", len(cs.Links))
+    cs.mu.Unlock()
 
-    parseHTML(site, websites, cs)
+    parseHTML(site, websites, db, cs, client)
   }
 
   return nil
@@ -70,21 +137,29 @@ func hashUrl(url string) uint64 {
 
 func maxWebsitesCrawled(cs *CrawledSet) (bool) {
   cs.mu.Lock()
-  if len(cs.Links) >= 5000 {
+  if len(cs.Links) >= 250 {
     cs.mu.Unlock()
     return true
-  } else {
+  } else { 
     cs.mu.Unlock()
     return false
   }
 }
 
-func parseHTML(site string, websites chan string, cs *CrawledSet) (err error) {
+func parseHTML(site string, websites chan string, db *DatabaseConnection, cs *CrawledSet, client *http.Client) (err error) {
   fmt.Println("parsing site: ", site)
 
-  res, err := http.Get(site)
+  req, err := http.NewRequest("GET", site, nil)
   if err != nil {
     fmt.Println("Error: failed to fetch/get site: ", site)
+    return err
+  }
+
+  req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36")
+  res, err := client.Do(req)
+
+  if err != nil {
+    fmt.Println("Error from client.Do", err) 
     return err
   }
 
@@ -105,7 +180,23 @@ func parseHTML(site string, websites chan string, cs *CrawledSet) (err error) {
       }
 
     case html.StartTagToken:
+      tt = z.Next()
       token := z.Token()
+      if len(token.Data) == 5 && token.Data == "title" {
+        z.Next()
+        token := z.Token()
+        website := Website{
+          Url: site,
+          Title: token.Data,
+        }
+
+        res, err := db.collection.InsertOne(context.TODO(), website)
+        if err != nil {
+          panic(err)
+        }
+        fmt.Println("res: ", res)
+      }
+
       if len(token.Data) == 1 && token.Data == "a" {
         url := ""
         for i := range token.Attr {
@@ -128,9 +219,12 @@ func parseHTML(site string, websites chan string, cs *CrawledSet) (err error) {
         cs.mu.Lock()
         hashedUrl := hashUrl(url)
         if _, ok := cs.Links[hashedUrl]; !ok {
-          cs.Links[hashedUrl] = struct{}{}
           cs.mu.Unlock()
           if !maxWebsitesCrawled(cs) {
+            fmt.Println("maxWebsitesCrawled: ", maxWebsitesCrawled(cs))
+            cs.mu.Lock()
+            cs.Links[hashedUrl] = struct{}{}
+            cs.mu.Unlock()
             go func() {
               websites <- url
             }()
